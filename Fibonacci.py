e3bc49f123a47139a1d4c24d7acc0f8816797108
lucabiagioli <61350189+lucabiagioli@users.noreply.github.com>
2021-03-04 15:23:27 +0100

    Fibonacci 2
    update fibonacci 2

--- Fibonacci.py
+++ Fibonacci.py
@@ -7,7 +7,7 @@ def Fibonacci(n):
     elif n==0: 
         return 0
     # Second Fibonacci number is 1 
-    elif n==1: 
+    elif n==4: 
         return 1
     else: 
         return Fibonacci(n-1)+Fibonacci(n-2) 
